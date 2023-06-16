c24acb849399ef3d78c6da3f8e3f6aa273d5e651
SYS41N <ysine764@gmail.com>
2023-06-15 18:59:49 -0700

    alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print a string
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
