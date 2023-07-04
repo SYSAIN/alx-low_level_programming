10a459bb1f84ae905647ded49077d4805a77d2ac
SYS41N <ysine764@gmail.com>
2023-07-03 19:55:07 -0700

    alx c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <inistd.h>
+
+/**
+*_putchar - writes the character c to stdout
+* @c: the character to print
+*
+* return: on success 1.
+* on error, -1 is returned.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
