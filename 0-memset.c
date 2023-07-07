96da2f8f65a5b3dd06d8457a3267f4c86d7d8645
SYS41N <ysine764@gmail.com>
2023-07-06 19:48:41 -0700

    recurtion

--- 0-memset.c
+++ 0-memset.c
@@ -1,18 +1,1 @@
-#include "main.h"
-
-/**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: number of bytes to be filled
- * Return: pointer to s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
-}
+test
--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,13 @@
 * @n: max bytes to use
 * @s: pointer to put the constant
 * @b: constant
-* return: pointer s
+* return: pointer @s
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int itr;
+
 	for (itr = 0; n > 0; itr++, n--)
 	{
 		s[itr] = b;
