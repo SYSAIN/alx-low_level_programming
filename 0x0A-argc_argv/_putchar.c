8e97aec895587ca32d928e29e15ec9fd9a97d50a
SYS41N <ysine764@gmail.com>
2023-07-07 20:03:14 -0700

    alx

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -1,10 +1,8 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -12,3 +10,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
