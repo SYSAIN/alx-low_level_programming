f61055bfe61d1acc39a11110da0509d0f1409b2f
SYS41N <ysine764@gmail.com>
2023-07-07 19:17:04 -0700

    argc/argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
