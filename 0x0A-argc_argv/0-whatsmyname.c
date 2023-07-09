d883c5d5478a063a6521e443006f1532a8b07b01
SYS41N <ysine764@gmail.com>
2023-07-09 12:49:32 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print program's name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+  */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
