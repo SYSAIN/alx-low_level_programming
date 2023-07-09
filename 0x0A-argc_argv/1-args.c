1ddd6fc269788aed6893c6e058e7165df9d0353b
SYS41N <ysine764@gmail.com>
2023-07-09 13:23:17 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 
-  */
+ * Return: 0
+ */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
