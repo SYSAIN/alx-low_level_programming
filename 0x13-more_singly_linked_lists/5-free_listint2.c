667fef7a6a02e313f6e65dad70bf47f6b431e4a6
SYSAIN <ysine764@gmail.com>
2023-12-11 09:38:05 -0800

    listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a singly linked list and set head to NULL
+ * @p_head: pointer to pointer to singly linked list head
+ */
+void free_listint2(listint_t **p_head)
+{
+	listint_t *current;
+	listint_t *h;
+
+	if (!p_head || *p_head == NULL)
+		return;
+
+	h = *p_head;
+	while (h)
+	{
+		current = h;
+		h = h->next;
+		free(current);
+	}
+
+	*p_head = NULL;
+}
